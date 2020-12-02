9b4858b0ee625ada438fe366461781b0c0958efe
jonathanFager <faaager@gmail.com>
2020-12-02 11:41:09 +0100

    Working solutions for day 2. Part1 3.0408ms, part2 1.167ms. Main 5.058ms

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -5,10 +5,8 @@ const main = async () => {
   const input = await readInputSplit(path.join(__dirname, "./input.txt"));
 
   console.time("main");
-    //part1();
-    part2();
-
-
+  part1();
+  part2();
 
   function part1() {
     console.time("part1");
@@ -31,7 +29,6 @@ const main = async () => {
         passes++;
       } else {
         i++;
-        console.log(i, "did not pass");
         //console.log("Lower is: ", lower, "Upper is: ", upper);
         //console.log("key is: ", key, "Pass is: ", password);
         //console.log("Count is:", count, "Pass is: ", password);
@@ -41,40 +38,26 @@ const main = async () => {
     console.timeEnd("part1");
   } //end part 1
 
-
-
   function part2() {
     console.time("part2");
     var passes: number = 0;
-    var i: number = 0;
     for (const raw of input) {
       const arrayOfStrings = raw.split(/[\s:-]+/, 4);
-      //console.log(arrayOfStrings);
-      const lower = parseInt(arrayOfStrings[0]);
-      const upper = parseInt(arrayOfStrings[1]);
       const key = arrayOfStrings[2];
       const password = arrayOfStrings[3];
-      //console.log("Lower is: ", lower, "Upper is: ", upper);
-      //console.log("key is: ", key, "Pass is: ", password);
-      var re = new RegExp(key, "g");
-      var count = (password.match(re) || []).length;
-      //console.log("Count is:", count, "Pass is: ", password);
-      if (count >= lower && count <= upper) {
-        i++;
-        passes++;
-      } else {
-        i++;
-        console.log(i, "did not pass");
-        //console.log("Lower is: ", lower, "Upper is: ", upper);
-        //console.log("key is: ", key, "Pass is: ", password);
-        //console.log("Count is:", count, "Pass is: ", password);
+      const lower = parseInt(arrayOfStrings[0]);
+      const upper = parseInt(arrayOfStrings[1]);
+      const lowerPosition = password.charAt(lower - 1);
+      const upperPosition = password.charAt(upper - 1);
+      if (lowerPosition == key || upperPosition == key) {
+        if (upperPosition != lowerPosition) {
+          passes++;
+        }
       }
     }
     console.log("Total passes:", passes);
     console.timeEnd("part2");
   } //end part 2
-
-
   console.timeEnd("main");
 };
 
